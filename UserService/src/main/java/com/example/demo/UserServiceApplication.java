b4a5f48d25a2f4a4519a59ef353c81be61f12d71
jonghyun.park.revature <jonghyun.park.revature>
2019-11-12 15:46:52 -0500

    commit for merge from brit

--- UserService/src/main/java/com/example/demo/UserServiceApplication.java
+++ UserService/src/main/java/com/example/demo/UserServiceApplication.java
@@ -2,12 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
 public class UserServiceApplication {
@@ -15,4 +11,5 @@ public class UserServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
+
 }
--- UserService/src/main/java/com/example/feign/JSONPlaceHolderClient.java
+++
@@ -1,5 +0,0 @@
-package com.example.feign;
-
-public interface JSONPlaceHolderClient {
-
-}
