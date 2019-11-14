7231c6802bd06231af96f187e6e67bae50a6f528
John Kelly <JohnThaddeusKelly@outlook.com>
2019-11-14 16:15:45 -0500

    saving

--- User-Service/src/test/java/com/revature/UserServiceApplicationTests.java
+++ User-Service/src/test/java/com/revature/UserServiceApplicationTests.java
@@ -4,21 +4,28 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.revature.controller.UserController;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest
+//@ContextConfiguration({"classpath*:applicationContext.xml"})
+//@SpringBootTest(classes=UserController.class)
 public class UserServiceApplicationTests {
-	UserController uc = new UserController();
+	@Autowired
+	private UserController controller;
 	@Test
 	public void userGetAllTest() {
-		assertEquals("bodhi", uc.getAll().getUsername());
+		assertEquals("bodhi", controller.getAll().getUsername());
 	}
+	@Test
 	public void userSaveTest() {
-		assertEquals("worked", uc.save());
+		assertEquals("worked", controller.save());
 	}
 
 }
