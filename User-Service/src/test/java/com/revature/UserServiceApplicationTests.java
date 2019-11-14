7debcb95268708f2f1d3cb3515c696c4d3154734
John Kelly <JohnThaddeusKelly@outlook.com>
2019-11-14 15:43:52 -0500

    I AM CURED

--- User-Service/src/test/java/com/revature/UserServiceApplicationTests.java
+++ User-Service/src/test/java/com/revature/UserServiceApplicationTests.java
@@ -1,16 +1,24 @@
 package com.revature;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.revature.controller.UserController;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserServiceApplicationTests {
-
+	UserController uc = new UserController();
 	@Test
-	public void contextLoads() {
+	public void userGetAllTest() {
+		assertEquals("bodhi", uc.getAll().getUsername());
+	}
+	public void userSaveTest() {
+		assertEquals("worked", uc.save());
 	}
 
 }
