3e8dc7e0850ca5957aab475a2fc84094ecabbb5c
jonghyun.park.revature <jonghyun.park.revature>
2019-11-14 12:46:38 -0500

    commiting commiting

--- User-Service/src/main/java/com/revature/controller/UserController.java
+++ User-Service/src/main/java/com/revature/controller/UserController.java
@@ -2,14 +2,19 @@ package com.revature.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.model.UserBean;
+import com.revature.service.UserRepository;
 
 @RestController
 public class UserController {
+	@Autowired
+	private UserRepository repo;
 	List<UserBean> list;
+	
 	
 	@GetMapping(value="/users")
 	public UserBean getAll() {
