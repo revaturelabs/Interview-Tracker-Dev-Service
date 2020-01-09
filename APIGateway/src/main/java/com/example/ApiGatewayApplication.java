9d5d650fa3f626089d6fce492d538fa92c8548c8
Ryan Kim <ryankeehoon@gmail.com>
2020-01-09 12:01:53 -0500

    commit
    1) added @Bean corsFilter() method in main drivers of ApiGateway (Zuul) and UserService
    2) fixed eureka client connectivity on application.properties by adding 3 lines

--- APIGateway/src/main/java/com/example/ApiGatewayApplication.java
+++ APIGateway/src/main/java/com/example/ApiGatewayApplication.java
@@ -9,6 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -19,5 +23,20 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
-
+	
+	@Bean
+	public CorsFilter corsFilter() {
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.addAllowedOrigin("*");
+	    config.addAllowedHeader("*");
+	    config.addAllowedMethod("*");
+	    source.registerCorsConfiguration("/**", config);
+	    
+	    System.out.println("got to bean cors filter");
+	    
+	    return new CorsFilter(source);
+	    
+	}	
 }
